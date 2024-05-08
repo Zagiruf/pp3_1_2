73f99d7a16d2f2c2d24f2c581f645785925f952c
Zagiruf <Zagiruf@gmail.com>
2024-05-08 16:39:01 +0500

    commmmmmmmttttее

--- src/main/java/web/pp3_1_2/model/User.java
+++ src/main/java/web/pp3_1_2/model/User.java
@@ -10,11 +10,8 @@ import java.util.Objects;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private int id;
-    @Column(name = "name")
     private String name;
-    @Column(name = "age")
     private int age;
 
     public User() {
